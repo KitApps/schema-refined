97e57c9bce187611077dd9cbdc9e5601a59c7924
Ivan Kryvoruchko <gildraug@gmail.com>
2018-06-18 15:47:21 +0300

    Add tests for ordering predicates

--- test/schema_refined/core_test.clj
+++ test/schema_refined/core_test.clj
@@ -52,7 +52,103 @@
            {:lat 51.5085 :lng -0.0762})
 
       (not-ok! (r/refined GeoPoint (r/On :lat r/NegativeDouble))
-               {:lat 47.3529 :lng 8.5199}))))
+               {:lat 47.3529 :lng 8.5199}))
+
+    (t/deftest refined-with-equal-predicate
+      (ok! (r/refined s/Int (r/Equal 42)) 42)
+      (ok! (r/refined s/Str (r/Equal "doom")) "doom")
+
+      (not-ok! (r/refined s/Int (r/Equal 42)) 43)
+      (not-ok! (r/refined s/Str (r/Equal "doom")) "Doom"))
+
+    (t/deftest refined-with-less-predicate
+      (ok! (r/refined s/Int (r/Less 108)) 42)
+      (ok! (r/refined double (r/Less 0.7)) 0.5)
+
+      (not-ok! (r/refined s/Int (r/Less 108)) 108)
+      (not-ok! (r/refined double (r/Less 0.7)) 3.14))
+
+    (t/deftest refined-with-less-or-equal-predicate
+      (ok! (r/refined s/Int (r/LessOrEqual 108)) 42)
+      (ok! (r/refined s/Int (r/LessOrEqual 108)) 108)
+      (ok! (r/refined double (r/LessOrEqual 0.7)) 0.7)
+
+      (not-ok! (r/refined s/Int (r/LessOrEqual 108)) 109)
+      (not-ok! (r/refined double (r/LessOrEqual 0.7)) 3.14))
+
+    (t/deftest refined-with-greater-predicate
+      (ok! (r/refined s/Int (r/Greater 42)) 108)
+      (ok! (r/refined double (r/Greater 0.5)) 0.7)
+
+      (not-ok! (r/refined s/Int (r/Greater 108)) 108)
+      (not-ok! (r/refined double (r/Greater 3.14)) 0.7))
+
+    (t/deftest refined-with-greater-or-equal-predicate
+      (ok! (r/refined s/Int (r/GreaterOrEqual 42)) 108)
+      (ok! (r/refined s/Int (r/GreaterOrEqual 108)) 108)
+      (ok! (r/refined double (r/GreaterOrEqual 0.7)) 0.7)
+
+      (not-ok! (r/refined s/Int (r/GreaterOrEqual 109)) 108)
+      (not-ok! (r/refined double (r/GreaterOrEqual 3.14)) 0.7))
+
+    (t/deftest refined-with-open-interval-predicate
+      (ok! (r/refined s/Int (r/OpenInterval 0 43)) 42)
+      (ok! (r/refined double (r/OpenInterval 0.0 1.0)) 0.7)
+      (ok! (r/refined s/Int (r/Epsilon 10 5)) 10)
+      (ok! (r/refined s/Int (r/Epsilon 10 5)) 13)
+      (ok! (r/refined s/Int (r/Epsilon 10 5)) 7)
+
+      (not-ok! (r/refined s/Int (r/OpenInterval 0 43)) 0)
+      (not-ok! (r/refined s/Int (r/OpenInterval 0 43)) 43)
+      (not-ok! (r/refined s/Int (r/OpenInterval 0 43)) -7)
+      (not-ok! (r/refined s/Int (r/OpenInterval 0 43)) 108)
+      (not-ok! (r/refined double (r/OpenInterval 0.0 1.0)) 0.0)
+      (not-ok! (r/refined double (r/OpenInterval 0.0 1.0)) 1.0)
+      (not-ok! (r/refined double (r/OpenInterval 0.0 1.0)) 3.14)
+      (not-ok! (r/refined double (r/OpenInterval 0.0 1.0)) -3.14)
+      (not-ok! (r/refined s/Int (r/Epsilon 10 5)) 5)
+      (not-ok! (r/refined s/Int (r/Epsilon 10 5)) 15)
+      (not-ok! (r/refined s/Int (r/Epsilon 10 5)) -7)
+      (not-ok! (r/refined s/Int (r/Epsilon 10 5)) 108))
+
+    (t/deftest refined-with-closed-interval-predicate
+      (ok! (r/refined s/Int (r/ClosedInterval 0 43)) 42)
+      (ok! (r/refined s/Int (r/ClosedInterval 0 43)) 0)
+      (ok! (r/refined s/Int (r/ClosedInterval 0 43)) 43)
+      (ok! (r/refined double (r/ClosedInterval 0.0 1.0)) 0.7)
+      (ok! (r/refined double (r/ClosedInterval 0.0 1.0)) 0.0)
+      (ok! (r/refined double (r/ClosedInterval 0.0 1.0)) 1.0)
+
+      (not-ok! (r/refined s/Int (r/ClosedInterval 0 43)) -7)
+      (not-ok! (r/refined s/Int (r/ClosedInterval 0 43)) 108)
+      (not-ok! (r/refined double (r/ClosedInterval 0.0 1.0)) 3.14)
+      (not-ok! (r/refined double (r/ClosedInterval 0.0 1.0)) -3.14))
+
+    (t/deftest refined-with-open-closed-interval-predicate
+      (ok! (r/refined s/Int (r/OpenClosedInterval 0 43)) 42)
+      (ok! (r/refined s/Int (r/OpenClosedInterval 0 43)) 43)
+      (ok! (r/refined double (r/OpenClosedInterval 0.0 1.0)) 0.7)
+      (ok! (r/refined double (r/OpenClosedInterval 0.0 1.0)) 1.0)
+
+      (not-ok! (r/refined s/Int (r/OpenClosedInterval 0 43)) -7)
+      (not-ok! (r/refined s/Int (r/OpenClosedInterval 0 43)) 108)
+      (not-ok! (r/refined s/Int (r/OpenClosedInterval 0 43)) 0)
+      (not-ok! (r/refined double (r/OpenClosedInterval 0.0 1.0)) 3.14)
+      (not-ok! (r/refined double (r/OpenClosedInterval 0.0 1.0)) -3.14)
+      (not-ok! (r/refined double (r/OpenClosedInterval 0.0 1.0)) 0.0))
+
+    (t/deftest refined-with-closed-open-interval-predicate
+      (ok! (r/refined s/Int (r/ClosedOpenInterval 0 43)) 42)
+      (ok! (r/refined s/Int (r/ClosedOpenInterval 0 43)) 0)
+      (ok! (r/refined double (r/ClosedOpenInterval 0.0 1.0)) 0.7)
+      (ok! (r/refined double (r/ClosedOpenInterval 0.0 1.0)) 0.0)
+
+      (not-ok! (r/refined s/Int (r/ClosedOpenInterval 0 43)) -7)
+      (not-ok! (r/refined s/Int (r/ClosedOpenInterval 0 43)) 108)
+      (not-ok! (r/refined s/Int (r/ClosedOpenInterval 0 43)) 43)
+      (not-ok! (r/refined double (r/ClosedOpenInterval 0.0 1.0)) 3.14)
+      (not-ok! (r/refined double (r/ClosedOpenInterval 0.0 1.0)) -3.14)
+      (not-ok! (r/refined double (r/ClosedOpenInterval 0.0 1.0)) 1.0))))
 
 (t/deftest validate-non-empty-values
   (ok! r/NonEmptyStr "doom")
