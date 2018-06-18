74691a9eb01e6fc8c050b30338809706c24e253f
Alexey Kachayev <kachayev@gmail.com>
2018-06-18 17:52:50 +0300

    Different profiles for different Clojure versions

--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
-(defproject com.attendify/schema-refined "0.3.0"
-  :description "Less bugs with precise and extendible schemas (refined types with runtime check)"
+(defproject com.attendify/schema-refined "0.3.0-alpha4"
+  :description "Clojure library to keep you away from bugs with precise schemas (refined types with runtime checks)"
   :Url "https://github.com/KitApps/schema-refined"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [prismatic/schema "1.1.9"]
-                 [potemkin "0.4.5"]])
+  :dependencies [[prismatic/schema "1.1.9"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
+                                  [potemkin "0.4.5"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [potemkin "0.4.5"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [potemkin "0.4.5"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-alpha4"]
+                                   [potemkin "0.4.5"]]}})
